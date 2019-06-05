171efbb2d9b9e8836731e364765c4cbce6e75865
garyparrot <weedbug666@gmail.com>
2019-06-05 20:38:21 +0800

    Implement a script for setup heroku

--- setup_heroku.py
+++ setup_heroku.py
@@ -1,24 +1,31 @@
-import sys, os, subprocess, re
+import sys, os, subprocess, re, hashlib
 
-print(sys.stdout.encoding)
 
 # test heroku
 if subprocess.run(['heroku', '--version']).returncode != 0:
     print("heroku not found")
     exit(1)
 
+print("Encoding: ", sys.stdout.encoding)
+account_id  = input('輸入你的圖書館證號: ')
+account_pwd = input('輸入你的圖書館密碼: ')
+line_token  = input('輸入你的Line Access token: ')
+line_secret = input("輸入你的Line Secret: ")
+
 # login heroku
-print('Heroku網頁等等會出現,請點登入')
-if subprocess.run(['heroku', 'login'],input=b'\n').returncode != 0:
+if subprocess.run(['heroku', 'login']).returncode != 0:
     print("login failed")
     exit(1)
 
-appname = 'yunlib-bot'
-dbname = 'yunlib-bot-db'
+content = "SHA256" + account_id + "_FUCK_PYTHON"
+sha256 = hashlib.sha256(content.encode('UTF-8')).hexdigest()[:9]
+appname = 'yunlib-bot-' + sha256
+dbname = 'yunlib-bot-db-' + sha256
 
 subprocess.run(['heroku', 'create',appname])
 subprocess.run(['heroku', 'labs:enable', 'runtime-dyno-metadata','-a',appname])
 subprocess.run(['heroku', 'addons:create', 'heroku-postgresql:hobby-dev', '-a', appname , '--name', dbname])
+subprocess.run(['heroku','git:remote','--app', appname])
 
 # retrive database config
 output = subprocess.check_output(['heroku', 'config:get', 'DATABASE_URL'])
@@ -41,22 +48,19 @@ result += subprocess.run(['heroku', 'config:set', 'DB_USER=%s' % db_username]).r
 result += subprocess.run(['heroku', 'config:set', 'DB_TYPE=%s' % "postgresql"]).returncode
 result += subprocess.run(['heroku', 'config:set', 'RUNNING_ENVIRONMENT=%s' % "DYNO"]).returncode
 
-line_token  = input('輸入你的Line Access token: ')
-line_secret = input("輸入你的Line Secret: ")
-account_id  = input('輸入你的圖書館證號: ')
-account_pwd = input('輸入你的圖書館密碼: ')
 
 if not (line_token == line_secret == account_id == account_pwd == ""):
     result += subprocess.run(['heroku', 'config:set', 'CHANNEL_ACCESS_TOKEN=%s' % line_token]).returncode
     result += subprocess.run(['heroku', 'config:set', 'CHANNEL_SECRET=%s' % line_secret]).returncode
     result += subprocess.run(['heroku', 'config:set', 'ACCOUNT_ID=%s' % account_id]).returncode
     result += subprocess.run(['heroku', 'config:set', 'ACCOUNT_PASSWORD=%s' % account_pwd]).returncode
+else:
+    print("Warning: 沒有提供帳號和Line token等資訊")
 
 if result != 0:
     print("failed to config some setting.")
     exit(0)
 
-subprocess.run(['heroku','git:remote','--app', appname])
 subprocess.run(['git', 'push', 'heroku', 'master'])
 domain = subprocess.check_output(['heroku', 'domains', '--app', appname]).decode(sys.stdout.encoding).split('\n')[1]
 print("https://%s" % domain)
